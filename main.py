169a721ede6d40e298fc34aa23a2ed7ae09ccf0a
Elena Lukyanchuk <e.lukyanchukk@gmail.com>
2024-11-06 21:32:49 +0100

    small changes

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from __future__ import unicode_literals
 import urllib.parse
 import urllib.request
 import re
@@ -6,7 +7,7 @@ category=input()
 
 category=category.strip()
 url = urllib.request.urlopen("https://pl.wikipedia.org/wiki/Kategoria:" + category.replace(" ", "_"))
-# print("URL: ", "https://pl.wikipedia.org/wiki/Kategoria:" + category.replace(" ", "_"))
+print("URL: ", "https://pl.wikipedia.org/wiki/Kategoria:" + category.replace(" ", "_"))
 mybytes = url.read()
 
 html_content = mybytes.decode("utf8")
@@ -38,18 +39,24 @@ html_content_art2 = bytes_art2.decode("utf8")
 art1_urlopen.close()
 art2_urlopen.close()
 
+print(url_art1, url_art2)
+
 # Linki wewnętrzne - art1
 
 internal_links_1 = []
 
 for link in re.findall(r'href="(\/wiki\/[^":#]*)"', html_content_art1):
-    if (":" in link) or ("https://pl.wikipedia.org/"+link == url_art1) or (link == '/wiki/Ziemia'): 
-        continue
+    if re.findall(r'href="(\/wiki\/[^":#]*)"', html_content_art1):
+        if (":" in link) or ("https://pl.wikipedia.org/"+link == url_art1) or (link == '/wiki/Ziemia'): 
+            continue
 
-    internal_links_1.append(urllib.parse.unquote(link).replace("_", " ").replace("/wiki/", ''))
+        internal_links_1.append(urllib.parse.unquote(link).replace("_", " ").replace("/wiki/", ''))
 
-    if len(internal_links_1) >= 5: 
-        break
+        if len(internal_links_1) >= 5: 
+            break
+
+    else: 
+        print("")
 
 print(" | ".join(internal_links_1))
 
@@ -58,60 +65,87 @@ print(" | ".join(internal_links_1))
 images_1 = []
 
 for link in re.findall(r"src=[\"']?(//[^\"'\s]+?\.(?:jpg|png)(?:\/[^\"'\s]+)?)[\"']?", html_content_art1):
-    if link == '//upload.wikimedia.org/wikipedia/commons/thumb/e/e9/Geographylogo.svg/20px-Geographylogo.svg.png':
-        continue
+    if re.findall(r"src=[\"']?(//[^\"'\s]+?\.(?:jpg|png)(?:\/[^\"'\s]+)?)[\"']?", html_content_art1): 
+        if link == '//upload.wikimedia.org/wikipedia/commons/thumb/e/e9/Geographylogo.svg/20px-Geographylogo.svg.png':
+            continue
 
-    images_1.append(urllib.parse.unquote(link))
+        images_1.append(urllib.parse.unquote(link))
 
-    if len(images_1) >= 3: 
-        break
+        if len(images_1) >= 3: 
+            break
+    
+    else: 
+        print("")
 
 print(" | ".join(images_1))
 
 # Linki zewnętrzne - art 1
 
-external_links_html = re.search(r'<div class="mw-references-wrap mw-references-columns">.*?</div>', html_content_art1, re.DOTALL)
-start, end = external_links_html.span()
-html_references = html_content_art1[start:end]
+external_links_html = re.search(r'<div class="mw-references-wrap.*?</div>', html_content_art1, re.DOTALL)
 
-external_links_1 = []
+if external_links_html: 
 
-for link in re.findall(r'href="(http[s]?://[^"]+)"', html_references): 
-    external_links_1.append(urllib.parse.unquote(link))
+    start, end = external_links_html.span()
+    html_references = html_content_art1[start:end]
 
-    if len(external_links_1) >= 3: 
-        break
+    external_links_1 = []
 
-print(" | ".join(external_links_1))
+    for link in re.findall(r'href="(http[s]?://[^"]+)"', html_references): 
+        if re.findall(r'href="(http[s]?://[^"]+)"', html_references):
+            external_links_1.append(urllib.parse.unquote(link))
+
+            if len(external_links_1) >= 3: 
+                break
+
+        else:
+            print("")
+
+    print(" | ".join(external_links_1))
+
+else: 
+    print("")
 
 # Kategorie - art 1
 
 categories_html = re.search(r'<div id="catlinks" class="catlinks" data-mw="interface"><div id="mw-normal-catlinks" class="mw-normal-catlinks">.*?</div></div>', html_content_art1, re.DOTALL)
-start, end = categories_html.span()
-html_references = html_content_art1[start:end]
 
-categories_1 = []
+if categories_html:
+    start, end = categories_html.span()
+    html_references = html_content_art1[start:end]
 
-for link in re.findall(r'title="Kategoria:([^"]+)"', html_references): 
-    categories_1.append(urllib.parse.unquote(link))
+    categories_1 = []
 
-    if len(categories_1) >= 3: 
-        break
+    for link in re.findall(r'title="Kategoria:([^"]+)"', html_references): 
+        if re.findall(r'title="Kategoria:([^"]+)"', html_references):
+            categories_1.append(urllib.parse.unquote(link))
 
-print(" | ".join(categories_1))
+            if len(categories_1) >= 3: 
+                break
+
+        else:
+            print("")
+
+    print(" | ".join(categories_1))
+
+else: 
+    print("")
 
 # Linki wewnętrzne - art2
 
 internal_links_2 = []
 
 for link in re.findall(r'href="(\/wiki\/[^":#]*)"', html_content_art2):
-    if (":" in link) or ("https://pl.wikipedia.org/"+link == url_art2) or (link == '/wiki/Ziemia'): 
-        continue
+    if re.findall(r'href="(\/wiki\/[^":#]*)"', html_content_art2):
+        if (":" in link) or ("https://pl.wikipedia.org/"+link == url_art2) or (link == '/wiki/Ziemia'): 
+            continue
 
-    internal_links_2.append(urllib.parse.unquote(link).replace("_", " ").replace("/wiki/", ''))
+        internal_links_2.append(urllib.parse.unquote(link).replace("_", " ").replace("/wiki/", ''))
 
-    if len(internal_links_2) >= 5: 
-        break
+        if len(internal_links_2) >= 5: 
+            break
+    
+    else: 
+        print("")
 
 print(" | ".join(internal_links_2))
 
@@ -120,44 +154,65 @@ print(" | ".join(internal_links_2))
 images_2 = []
 
 for link in re.findall(r"src=[\"']?(//[^\"'\s]+?\.(?:jpg|png)(?:\/[^\"'\s]+)?)[\"']?", html_content_art2):
-    if link == '//upload.wikimedia.org/wikipedia/commons/thumb/e/e9/Geographylogo.svg/20px-Geographylogo.svg.png':
-        continue
+    if re.findall(r"src=[\"']?(//[^\"'\s]+?\.(?:jpg|png)(?:\/[^\"'\s]+)?)[\"']?", html_content_art2):
+        if link == '//upload.wikimedia.org/wikipedia/commons/thumb/e/e9/Geographylogo.svg/20px-Geographylogo.svg.png':
+            continue
 
-    images_2.append(urllib.parse.unquote(link))
+        images_2.append(urllib.parse.unquote(link))
 
-    if len(images_2) >= 3: 
-        break
+        if len(images_2) >= 3: 
+            break
+
+    else:
+        print("")
 
 print(" | ".join(images_2))
 
 # Linki zewnętrzne - art 2
 
-external_links_html = re.search(r'<div class="mw-references-wrap mw-references-columns">.*?</div>', html_content_art2, re.DOTALL)
-start, end = external_links_html.span()
-html_references = html_content_art2[start:end]
+external_links_html = re.search(r'<div class="mw-references-wrap.*?</div>', html_content_art2, re.DOTALL)
 
-external_links_2 = []
+if external_links_html:
+    start, end = external_links_html.span()
+    html_references = html_content_art2[start:end]
 
-for link in re.findall(r'href="(http[s]?://[^"]+)"', html_references): 
-    external_links_2.append(urllib.parse.unquote(link))
+    external_links_2 = []
 
-    if len(external_links_2) >= 3: 
-        break
+    for link in re.findall(r'href="(http[s]?://[^"]+)"', html_references): 
+        if re.findall(r'href="(http[s]?://[^"]+)"', html_references):
+            external_links_2.append(urllib.parse.unquote(link))
 
-print(" | ".join(external_links_2))
+            if len(external_links_2) >= 3: 
+                break
 
+        else:
+            print("")
+
+    print(" | ".join(external_links_2))
+
+else:
+    print("")
 # Kategorie - art 2
 
 categories_html = re.search(r'<div id="catlinks" class="catlinks" data-mw="interface"><div id="mw-normal-catlinks" class="mw-normal-catlinks">.*?</div></div>', html_content_art2, re.DOTALL)
-start, end = categories_html.span()
-html_references = html_content_art2[start:end]
 
-categories_2 = []
+if categories_html:
+    start, end = categories_html.span()
+    html_references = html_content_art2[start:end]
 
-for link in re.findall(r'title="Kategoria:([^"]+)"', html_references): 
-    categories_2.append(urllib.parse.unquote(link))
+    categories_2 = []
 
-    if len(categories_2) >= 3: 
-        break
+    for link in re.findall(r'title="Kategoria:([^"]+)"', html_references): 
+        if re.findall(r'title="Kategoria:([^"]+)"', html_references):
+            categories_2.append(urllib.parse.unquote(link))
 
-print(" | ".join(categories_2))
+            if len(categories_2) >= 3: 
+                break
+
+        else: 
+            print("")
+
+    print(" | ".join(categories_2))
+
+else:
+    print("")
